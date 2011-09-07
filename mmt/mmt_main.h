c3f8990d970c7c3933b4fff9a7fbbd3fa50d2778
Marcin Slusarz <marcin.slusarz@gmail.com>
2011-09-07 19:39:30 +0200

    mmt: move instrumentation functions to separate file

--- mmt/mmt_main.h
+++ mmt/mmt_main.h
@@ -3,6 +3,10 @@
 
 #include "pub_tool_basics.h"
 
+#ifdef __LP64__
+#define MMT_64BIT
+#endif
+
 #define MMT_MAX_REGIONS 100
 
 struct mmt_mmap_data {
@@ -20,5 +24,28 @@ extern int mmt_last_region;
 extern UInt current_item;
 
 void mmt_free_region(int idx);
+
+VG_REGPARM(2)
+void trace_store(Addr addr, SizeT size, Addr inst_addr, UWord value);
+
+VG_REGPARM(2)
+void trace_store2(Addr addr, SizeT size, Addr inst_addr, UWord value1, UWord value2);
+
+#ifndef MMT_64BIT
+VG_REGPARM(2)
+void trace_store4(Addr addr, Addr inst_addr, UWord value1, UWord value2, UWord value3, UWord value4);
+#endif
+
+VG_REGPARM(2)
+void trace_load(Addr addr, SizeT size, UInt inst_addr, UWord value);
+
+VG_REGPARM(2)
+void trace_load2(Addr addr, SizeT size, UInt inst_addr, UWord value1, UWord value2);
+
+#ifndef MMT_64BIT
+VG_REGPARM(2)
+void trace_load4(Addr addr, SizeT size, UInt inst_addr, UWord value1, UWord value2, UWord value3, UWord value4);
+#endif
+
 
 #endif /* MMT_MAIN_H_ */
