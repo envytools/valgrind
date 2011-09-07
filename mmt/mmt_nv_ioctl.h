f003a5b8cfe23c33743b1c6953b5df7423ed1e75
Marcin Slusarz <marcin.slusarz@gmail.com>
2011-09-07 19:20:24 +0200

    mmt: move nvidia ioctl tracing to separate file

---
+++ mmt/mmt_nv_ioctl.h
@@ -0,0 +1,22 @@
+#ifndef MMT_NVIDIA_IOCTL_H_
+#define MMT_NVIDIA_IOCTL_H_
+
+#include "pub_tool_basics.h"
+
+extern int mmt_trace_nvidia_ioctls;
+extern int mmt_trace_marks;
+
+void mmt_nv_ioctl_fini(void);
+void mmt_nv_ioctl_post_clo_init(void);
+
+void mmt_nv_ioctl_post_open(UWord *args, SysRes res);
+void mmt_nv_ioctl_post_close(UWord *args);
+
+void mmt_nv_ioctl_post_mmap(UWord *args, SysRes res, int offset_unit);
+
+void mmt_nv_ioctl_pre(UWord *args);
+void mmt_nv_ioctl_post(UWord *args);
+
+void mmt_nv_ioctl_pre_clo_init(void);
+
+#endif /* NVIDIA_IOCTL_H_ */
