3e397a7f513b6211fc4a10dcc30995abae65aff2
Marcin Slusarz <marcin.slusarz@gmail.com>
2012-04-01 22:51:07 +0200

    mmt: add support for tracing nouveau ioctls

---
+++ mmt/mmt_nouveau_ioctl.h
@@ -0,0 +1,15 @@
+#ifndef MMT_NOUVEAU_IOCTL_H_
+#define MMT_NOUVEAU_IOCTL_H_
+
+#include "pub_tool_basics.h"
+
+extern int mmt_trace_nouveau_ioctls;
+
+void mmt_nouveau_ioctl_post_open(UWord *args, SysRes res);
+void mmt_nouveau_ioctl_post_close(UWord *args);
+void mmt_nouveau_ioctl_pre(UWord *args);
+void mmt_nouveau_ioctl_post(UWord *args);
+void mmt_nouveau_ioctl_pre_clo_init(void);
+
+
+#endif
