568bd7cd12fbe8ac856eea486a437551e0d86eb2
Marcin Ślusarz <marcin.slusarz@gmail.com>
2014-05-19 21:38:21 +0200

    mmt: port to valgrind 3.9

--- mmt/mmt_instrument.h
+++ mmt/mmt_instrument.h
@@ -8,6 +8,7 @@ IRSB *mmt_instrument(VgCallbackClosure *closure,
 				IRSB *bbIn,
 				VexGuestLayout *layout,
 				VexGuestExtents *vge,
+				VexArchInfo* archinfo_host,
 				IRType gWordTy, IRType hWordTy);
 
 #endif /* MMT_INSTRUMENT_H_ */
