ce573452e0b95f7c61ca1002555c514006f0d0db
Marcin Ślusarz <marcin.slusarz@gmail.com>
2014-09-06 22:17:19 +0200

    mmt: dump more ioctl data: mthd arguments and other ioctls

--- mmt/mmt_nv_ioctl.c
+++ mmt/mmt_nv_ioctl.c
@@ -127,11 +127,14 @@ static Addr release_nvidia_mapping2(UWord data1, UWord data2)
 
 static void dumpmem(const char *s, Addr addr, UInt size)
 {
+	if (!addr || !size)
+		return;
+
 	mmt_bin_write_1('n');
 	mmt_bin_write_1('o');
 	mmt_bin_write_8(addr);
 
-	if (!addr || (addr & 0xffff0000) == 0xbeef0000)
+	if ((addr & 0xffff0000) == 0xbeef0000)
 	{
 		mmt_bin_write_str("");
 		mmt_bin_write_buffer((UChar *)"", 0);
@@ -211,107 +214,33 @@ int mmt_nv_ioctl_post_mmap(UWord *args, SysRes res, int offset_unit)
 
 static struct object_type {
 	UInt id;		// type id
-	const char *name;		// some name
 	UInt cargs;		// number of constructor args (uint32)
 } object_types[] =
 {
-	{0x0000, "NV_CONTEXT_NEW", 0},
-
-	{0x0004, "NV_PTIMER", 0},
-
-	{0x0041, "NV_CONTEXT", 0},
-
-	{0x502d, "NV50_2D", 0},
-	{0x902d, "NVC0_2D", 0},
-
-	{0x5039, "NV50_M2MF", 0},
-	{0x9039, "NVC0_M2MF", 0},
-
-	{0x9068, "NVC0_PEEPHOLE", 0},
-
-	{0x406e, "NV40_FIFO_DMA", 6},
-
-	{0x506f, "NV50_FIFO_IB", 6},
-	{0x826f, "NV84_FIFO_IB", 6},
-	{0x906f, "NVC0_FIFO_IB", 6},
-
-	{0x5070, "NV84_DISPLAY", 4},
-	{0x8270, "NV84_DISPLAY", 4},
-	{0x8370, "NVA0_DISPLAY", 4},
-	{0x8870, "NV98_DISPLAY", 4},
-	{0x8570, "NVA3_DISPLAY", 4},
-
-	{0x5072, NULL, 8},
-
-	{0x7476, "NV84_VP", 0},
-
-	{0x507a, "NV50_DISPLAY_CURSOR", 0},
-	{0x827a, "NV84_DISPLAY_CURSOR", 0},
-	{0x857a, "NVA3_DISPLAY_CURSOR", 0},
-
-	{0x507b, "NV50_DISPLAY_OVERLAY", 0},
-	{0x827b, "NV84_DISPLAY_OVERLAY", 0},
-	{0x857b, "NVA3_DISPLAY_OVERLAY", 0},
-
-	{0x507c, "NV50_DISPLAY_SYNC_FIFO", 8},
-	{0x827c, "NV84_DISPLAY_SYNC_FIFO", 8},
-	{0x837c, "NVA0_DISPLAY_SYNC_FIFO", 8},
-	{0x857c, "NVA3_DISPLAY_SYNC_FIFO", 8},
-
-	{0x507d, "NV50_DISPLAY_MASTER_FIFO", 0},
-	{0x827d, "NV84_DISPLAY_MASTER_FIFO", 0},
-	{0x837d, "NVA0_DISPLAY_MASTER_FIFO", 0},
-	{0x887d, "NV98_DISPLAY_MASTER_FIFO", 0},
-	{0x857d, "NVA3_DISPLAY_MASTER_FIFO", 0},
-
-	{0x307e, "NV30_PEEPHOLE", 0},
-
-	{0x507e, "NV50_DISPLAY_OVERLAY_FIFO", 8},
-	{0x827e, "NV84_DISPLAY_OVERLAY_FIFO", 8},
-	{0x837e, "NVA0_DISPLAY_OVERLAY_FIFO", 8},
-	{0x857e, "NVA3_DISPLAY_OVERLAY_FIFO", 8},
-
-	{0x0080, "NV_DEVICE", 1},
-	{0x2080, "NV_SUBDEVICE_0", 0},
-	{0x2081, "NV_SUBDEVICE_1", 0},
-	{0x2082, "NV_SUBDEVICE_2", 0},
-	{0x2083, "NV_SUBDEVICE_3", 0},
-
-	{0x5097, "NV50_3D", 0},
-	{0x8297, "NV84_3D", 0},
-	{0x8397, "NVA0_3D", 0},
-	{0x8597, "NVA3_3D", 0},
-	{0x8697, "NVAF_3D", 0},
-	{0x9097, "NVC0_3D", 0},
-
-	{0x74b0, "NV84_BSP", 0},
-
-	{0x88b1, "NV98_BSP", 0},
-	{0x85b1, "NVA3_BSP", 0},
-	{0x86b1, "NVAF_BSP", 0},
-	{0x90b1, "NVC0_BSP", 0},
-
-	{0x88b2, "NV98_VP", 0},
-	{0x85b2, "NVA3_VP", 0},
-	{0x90b2, "NVC0_VP", 0},
-
-	{0x88b3, "NV98_PPP", 0},
-	{0x85b3, "NVA3_PPP", 0},
-	{0x90b3, "NVC0_PPP", 0},
-
-	{0x88b4, "NV98_CRYPT", 0},
-
-	{0x85b5, "NVA3_COPY", 0},
-	{0x90b5, "NVC0_COPY0", 0},
-
-	{0x50c0, "NV50_COMPUTE", 0},
-	{0x85c0, "NVA3_COMPUTE", 0},
-	{0x90c0, "NVC0_COMPUTE", 0},
-
-	{0x74c1, "NV84_CRYPT", 0},
-
-	{0x50e0, "NV50_PGRAPH", 0},
-	{0x50e2, "NV50_PFIFO", 0},
+	{0x0000, 1},
+	{0x0005, 6},
+	{0x0019, 4}, // +NULL
+	{0x0039, 4}, // +NULL
+	{0x0041, 1},
+	{0x0043, 4}, // +NULL
+	{0x0044, 4}, // +NULL
+	{0x004a, 4}, // +NULL
+	{0x3062, 4}, // +NULL
+	{0x3066, 4}, // +NULL
+	{0x406e, 8},
+	{0x0072, 4}, // +NULL
+	{0x0073, 0},
+	{0x0079, 6},
+	{0x307b, 4}, // +NULL
+	{0x357c, 3}, // +NULL
+	{0x0080, 8},
+	{0x2080, 1},
+	{0x3089, 4}, // +NULL
+	{0x308a, 4}, // +NULL
+	{0x9096, 0},
+	{0x4097, 4}, // +NULL
+	{0x309e, 4}, // +NULL
+	{0x009f, 4}, // +NULL
 };
 
 static struct object_type *find_objtype(UInt id)
@@ -326,6 +255,84 @@ static struct object_type *find_objtype(UInt id)
 	return NULL;
 }
 
+static void handle_nvrm_ioctl_call(struct nvrm_ioctl_call *s, int in)
+{
+	void *ptr = (void *)(unsigned long)s->ptr;
+	const char *str;
+	if (in)
+		str = "in";
+	else
+		str = "out";
+
+	dumpmem(str, s->ptr, s->size);
+
+	if (s->mthd == 0x10000002)
+	{
+		UInt *addr2 = ptr;
+		dumpmem(str, addr2[2], 0x3c);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_BAR0)
+	{
+		struct nvrm_mthd_subdevice_bar0 *m = ptr;
+		dumpmem(str, m->ptr, m->cnt * 4 * 8);
+	}
+	else if (s->mthd == NVRM_MTHD_DEVICE_GET_CLASSES)
+	{
+		struct nvrm_mthd_device_get_classes *m = ptr;
+		dumpmem(str, m->ptr, m->cnt * 4);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_FB_GET_PARAMS)
+	{
+		struct nvrm_mthd_subdevice_fb_get_params *m = ptr;
+		dumpmem(str, m->ptr, m->cnt * 4 * 2);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_BUS_GET_PARAMS)
+	{
+		struct nvrm_mthd_subdevice_bus_get_params *m = ptr;
+		dumpmem(str, m->ptr, m->cnt * 4 * 2);
+	}
+	else if (s->mthd == NVRM_MTHD_DEVICE_UNK1401)
+	{
+		struct nvrm_mthd_device_unk1401 *m = ptr;
+		dumpmem(str, m->ptr, m->cnt);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_GET_FIFO_ENGINES)
+	{
+		struct nvrm_mthd_subdevice_get_fifo_engines *m = ptr;
+		dumpmem(str, m->ptr, m->cnt * 4);
+	}
+	else if (s->mthd == NVRM_MTHD_DEVICE_UNK1102)
+	{
+		struct nvrm_mthd_device_unk1102 *m = ptr;
+		dumpmem(str, m->ptr, m->cnt);
+	}
+	else if (s->mthd == NVRM_MTHD_DEVICE_UNK1701)
+	{
+		struct nvrm_mthd_device_unk1701 *m = ptr;
+		dumpmem(str, m->ptr, m->cnt);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_UNK1201)
+	{
+		struct nvrm_mthd_subdevice_unk1201 *m = ptr;
+		dumpmem(str, m->ptr, m->cnt * 4 * 2);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_UNK0101)
+	{
+		struct nvrm_mthd_subdevice_unk0101 *m = ptr;
+		dumpmem(str, m->ptr, m->cnt * 4 * 2);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_UNK0522)
+	{
+		struct nvrm_mthd_subdevice_unk0522 *m = ptr;
+		dumpmem(str, m->ptr, m->size);
+	}
+	else if (s->mthd == NVRM_MTHD_SUBDEVICE_UNK0512)
+	{
+		struct nvrm_mthd_subdevice_unk0512 *m = ptr;
+		dumpmem(str, m->ptr, m->size);
+	}
+}
+
 void mmt_nv_ioctl_pre(UWord *args)
 {
 	int fd = args[0];
@@ -334,6 +341,8 @@ void mmt_nv_ioctl_pre(UWord *args)
 	UInt size;
 
 	if (!FD_ISSET(fd, &nvidiactl_fds) && !FD_ISSET(fd, &nvidia0_fds))
+		return;
+	if (!data)
 		return;
 
 	if (mmt_trace_marks)
@@ -377,39 +386,32 @@ void mmt_nv_ioctl_pre(UWord *args)
 		case NVRM_IOCTL_UNK38:
 		{
 			struct nvrm_ioctl_unk38 *s = (void *)data;
-			dumpmem("in ", s->ptr, s->unk18);
+			dumpmem("in ", s->ptr, s->size);
 			break;
 		}
 		case NVRM_IOCTL_QUERY:
 		{
 			struct nvrm_ioctl_query *s = (void *)data;
-			dumpmem("in ", s->ptr, s->status);//should be s/status/size/ ?
+			dumpmem("in ", s->ptr, s->size);
 			break;
 		}
 		case NVRM_IOCTL_CALL:
 		{
-			struct nvrm_ioctl_call *s = (void *)data;
-
-			dumpmem("in ", s->ptr, s->size);
-
-			if (s->mthd == 0x10000002)
-			{
-				UInt *addr2 = (UInt *) (unsigned long)s->ptr;
-				dumpmem("in2 ", addr2[2], 0x3c);
-			}
-			else if (s->mthd == NVRM_MTHD_SUBDEVICE_BAR0)
-			{
-				struct nvrm_mthd_subdevice_bar0 *m = (void *)(unsigned long)s->ptr;
-				UInt *tx = (UInt *)(unsigned long)m->ptr;
-
-				mmt_bin_write_1('n');
-				mmt_bin_write_1('1');
-				mmt_bin_write_4(m->cnt);
-				mmt_bin_write_8(m->ptr);
-				mmt_bin_write_buffer((UChar *)tx, m->cnt * 8 * 4);
-				mmt_bin_end();
-			}
-
+			handle_nvrm_ioctl_call((void *)data, 1);
+			break;
+		}
+		case NVRM_IOCTL_UNK41:
+		{
+			struct nvrm_ioctl_unk41 *s = (void *)data;
+			dumpmem("in", s->ptr1, 4);
+			dumpmem("in", s->ptr2, 4);
+			dumpmem("in", s->ptr3, 4);
+			break;
+		}
+		case NVRM_IOCTL_UNK4D:
+		{
+			struct nvrm_ioctl_unk4d *s = (void *)data;
+			dumpmem("in", s->sptr, s->slen);
 			break;
 		}
 		case NVRM_IOCTL_UNK4D_OLD:
@@ -422,12 +424,17 @@ void mmt_nv_ioctl_pre(UWord *args)
 			mmt_bin_end();
 			break;
 		}
-
+		case NVRM_IOCTL_UNK52:
+		{
+			struct nvrm_ioctl_unk52 *s = (void *)data;
+			dumpmem("in", s->ptr, 8);
+			break;
+		}
 		case NVRM_IOCTL_UNK5E:
 		{
 			// Copy data from mem to GPU
 			struct nvrm_ioctl_unk5e *s = (void *)data;
-			if (0) // enable after veryfing size
+			if (0)
 				dumpmem("in", s->ptr, 0x01000000);
 			break;
 		}
@@ -436,20 +443,15 @@ void mmt_nv_ioctl_pre(UWord *args)
 			struct nvrm_ioctl_create *s = (void *)data;
 			struct object_type *objtype = find_objtype(s->cls);
 
-			if (s->ptr)
-			{
-				if (objtype)
-					dumpmem("in ", s->ptr, objtype->cargs * 4);
-				else
-					dumpmem("in ", s->ptr, 0x40);
-			}
+			if (s->ptr && objtype)
+				dumpmem("in ", s->ptr, objtype->cargs * 4);
 
 			break;
 		}
 	}
 }
 
-void mmt_nv_ioctl_post(UWord *args)
+void mmt_nv_ioctl_post(UWord *args, SysRes res)
 {
 	int fd = args[0];
 	UInt id = args[1];
@@ -459,6 +461,11 @@ void mmt_nv_ioctl_post(UWord *args)
 
 	if (!FD_ISSET(fd, &nvidiactl_fds) && !FD_ISSET(fd, &nvidia0_fds))
 		return;
+	if (!data)
+		return;
+
+	if (sr_Res(res) || sr_isError(res))
+		VG_(message)(Vg_UserMsg, "ioctl result: %ld (0x%lx), iserr: %d, err: %ld (0x%lx)\n", sr_Res(res), sr_Res(res), sr_isError(res), sr_Err(res), sr_Err(res));
 
 	if (mmt_trace_marks)
 	{
@@ -540,34 +547,33 @@ void mmt_nv_ioctl_post(UWord *args)
 		}
 		case NVRM_IOCTL_CALL:
 		{
-			struct nvrm_ioctl_call *s = (void *)data;
-
-			dumpmem("out ", s->ptr, s->size);
-
-			if (data[2] == 0x10000002)
-			{
-				UInt *addr2 = (UInt *) (unsigned long)s->ptr;
-				dumpmem("out2 ", addr2[2], 0x3c);
-			}
-			else if (data[2] == NVRM_MTHD_SUBDEVICE_BAR0)
-			{
-				struct nvrm_mthd_subdevice_bar0 *m = (void *)(unsigned long)s->ptr;
-				UInt *tx = (UInt *)(unsigned long)m->ptr;
-
-				mmt_bin_write_1('n');
-				mmt_bin_write_1('1');
-				mmt_bin_write_4(m->cnt);
-				mmt_bin_write_8(m->ptr);
-				mmt_bin_write_buffer((UChar *)tx, m->cnt * 8 * 4);
-				mmt_bin_end();
-			}
-
+			handle_nvrm_ioctl_call((void *)data, 0);
+			break;
+		}
+		case NVRM_IOCTL_UNK41:
+		{
+			struct nvrm_ioctl_unk41 *s = (void *)data;
+			dumpmem("out", s->ptr1, 4);
+			dumpmem("out", s->ptr2, 4);
+			dumpmem("out", s->ptr3, 4);
+			break;
+		}
+		case NVRM_IOCTL_UNK4D:
+		{
+			struct nvrm_ioctl_unk4d *s = (void *)data;
+			dumpmem("out", s->sptr, s->slen);
 			break;
 		}
 		case NVRM_IOCTL_UNK38:
 		{
 			struct nvrm_ioctl_unk38 *s = (void *)data;
-			dumpmem("out", s->ptr, s->unk18);
+			dumpmem("out", s->ptr, s->size);
+			break;
+		}
+		case NVRM_IOCTL_UNK52:
+		{
+			struct nvrm_ioctl_unk52 *s = (void *)data;
+			dumpmem("out", s->ptr, 8);
 			break;
 		}
 		case NVRM_IOCTL_QUERY:
@@ -575,10 +581,20 @@ void mmt_nv_ioctl_post(UWord *args)
 			struct nvrm_ioctl_query *s = (void *)data;
 
 			UInt *addr2 = (UInt *)(unsigned long)s->ptr;
-			dumpmem("out", s->ptr, s->status); // s/status/size/ ?
+			dumpmem("out", s->ptr, s->size);
 			if (s->query == 0x14c && addr2[2])
 				// List supported object types
 				dumpmem("out2 ", addr2[2], addr2[0] * 4);
+
+			break;
+		}
+		case NVRM_IOCTL_CREATE:
+		{
+			struct nvrm_ioctl_create *s = (void *)data;
+			struct object_type *objtype = find_objtype(s->cls);
+
+			if (s->ptr && objtype)
+				dumpmem("out", s->ptr, objtype->cargs * 4);
 
 			break;
 		}
