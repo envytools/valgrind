6849aa7c1fa3868464008ab0e7946401b1c2b609
Marcin Ślusarz <marcin.slusarz@gmail.com>
2014-09-11 18:04:33 +0200

    mmt/nvidia: create pointer fuzzer and use it to detect ioctl_create argument size

--- mmt/mmt_nv_ioctl.h
+++ mmt/mmt_nv_ioctl.h
@@ -5,6 +5,16 @@
 
 extern int mmt_trace_nvidia_ioctls;
 extern int mmt_trace_marks;
+extern int mmt_ioctl_create_fuzzer;
+
+struct nv_object_type
+{
+	UInt id;		// type id
+	UInt cargs;		// number of constructor args (uint32)
+};
+
+extern struct nv_object_type mmt_nv_object_types[];
+extern int mmt_nv_object_types_count;
 
 void mmt_nv_ioctl_fini(void);
 void mmt_nv_ioctl_post_clo_init(void);
