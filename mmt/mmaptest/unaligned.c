d0c4613c77c908a321f78b16b1c2aa9b8bb31197
Marcin Ślusarz <marcin.slusarz@gmail.com>
2014-09-26 23:00:55 +0200

    mmt: capture writes between end of mmaped region and end of last page
    ... and add a test case

---
+++ mmt/mmaptest/unaligned.c
@@ -0,0 +1,18 @@
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void)
+{
+	int fd = open("/dev/zero", O_RDWR);
+	volatile uint32_t *v1 = mmap(NULL, 0x200, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+	v1[0x100/4] = 0x1234567;
+	v1[0x400/4] = 0xabcdef; // gets lost. nvidia regrettably do this
+	close(fd);
+	return 0;
+}
+
