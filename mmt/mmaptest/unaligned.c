3771d802443812ad65b1943bd661bc482f5e654b
Marcin Ślusarz <marcin.slusarz@gmail.com>
2014-11-08 09:46:09 +0100

    mmt/mmaptest: updates

--- mmt/mmaptest/unaligned.c
+++ mmt/mmaptest/unaligned.c
@@ -11,7 +11,7 @@ int main(void)
 	int fd = open("/dev/zero", O_RDWR);
 	volatile uint32_t *v1 = mmap(NULL, 0x200, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 	v1[0x100/4] = 0x1234567;
-	v1[0x400/4] = 0xabcdef; // gets lost. nvidia regrettably do this
+	v1[0x400/4] = 0xabcdef; // gets lost
 	close(fd);
 	return 0;
 }
